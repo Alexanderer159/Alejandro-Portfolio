8761b16e6a5a296e1f988139d4023ec7f49a431c
Alejandro De Yavorsky <aledeyavorsky@gmail.com>
2025-08-18 17:31:16 +0200

    Second

---
+++ src/Home.jsx
@@ -0,0 +1,17 @@
+import './Home.css'
+
+export const Home = () => {
+  return (
+    <>
+    <div className="container-fluid">
+      <img src="" alt="pic"/>
+      <h1>Alejandro De Yavorsky</h1>
+      <ul>
+        <li>Projects</li>
+        <li>work exp</li>
+        <li>Show off CSS Skills</li>
+      </ul>
+    </div>
+    </>
+  )
+}
