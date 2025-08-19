c66432da2ac3c5db7706286dcb1feb4abf6873a2
Alejandro De Yavorsky <aledeyavorsky@gmail.com>
2025-08-19 13:33:47 +0200

    pretty arrow

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -9,7 +9,7 @@ export const Home = () => {
           <img src="/Foto-CV.jpg" alt="pic" className="propic"/>
           <div className="info p-5">
             <p className="name m-0">Alejandro De Yavorsky</p>
-            <p className="devtext m-0">Full-Stack developer + Love for UI & UX = Amazing, eye-catching web-pages.</p>
+            <p className="devtext m-0">Full-Stack developer + Love for UI & UX = Amazing web-pages.</p>
             <div className="d-flex gap-3 pt-4 mt-4">
               <button className="buttonCV p-2">Download My CV</button>
               <button className="buttonWA p-2">Whatsapp</button>
@@ -17,6 +17,14 @@ export const Home = () => {
           </div>
         </div>
       </div>
+      <div className="d-flex justify-content-center">
+        <a data-scroll href="#projects">
+          <div class="arrow"></div>
+        </a>
+      </div>
+<div id="projects" className="d-flex justify-content-center mt-5 pt-5">
+  <p className="protitle">Projects</p>
+</div>
     </div>
     </>
   )
