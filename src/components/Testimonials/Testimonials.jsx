5f8f98a3447d5f69bcc0b7d8b0958d70e490bf57
Alejandro De Yavorsky <aledeyavorsky@gmail.com>
2025-09-09 02:22:31 +0200

    Looking pretty

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -14,9 +14,9 @@ export const Testimonials = () => {
                 <img src="Testimonials/Hans.png" className="testi-pic my-2"/>
                 
                 <p className="testi-name mt-3 mb-0">Hans Christian Aparicio Moreno</p>
-                <p className="testi-jobtitle mb-3">Senior Full-Stack Developer</p>
-                <p className="testi-text text-center my-3 px-3">"Alejandro is known for his attention to detail, ensuring that each delivery meets high standards of quality and usability. His adaptability and commitment to the project were key to achieving the established objectives. Without a doubt, he is a valuable professional who would contribute greatly to any team he works on."</p>
-                <a href="https://www.linkedin.com/in/hansaparicio/" className="testi-link text-white p-2">Check veracity</a>
+                <p className="testi-jobtitle mb-3"><strong className="emphasis">Senior Full-Stack Developer</strong></p>
+                <p className="testi-text text-center my-3 px-3"><strong className="emphasis">"</strong>Alejandro is known for his attention to detail, ensuring that each delivery meets high standards of quality and usability. His adaptability and commitment to the project were key to achieving the established objectives. Without a doubt, he is a valuable professional who would contribute greatly to any team he works on.<strong className="emphasis">"</strong></p>
+                <a href="https://www.linkedin.com/in/hansaparicio/" className="testi-link p-2">Check veracity</a>
 
             </div>
         </div>
